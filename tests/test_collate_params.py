c4715b9da98911b034f6424959a65af594e98053
curriem <miles.h.currie@nasa.gov>
2025-01-30 15:17:26 -0500

    simple test now working

--- tests/test_collate_params.py
+++ tests/test_collate_params.py
@@ -3,13 +3,14 @@ from eacy import collate
 
 class TestUtils(unittest.TestCase):
     def test_collate(self):
+        import pickle
+        params_to_compare = pickle.load(open("eacy_params.pk", "rb"))
         telescope_name = "EAC1"
         instrument_name = "CI"
         detector_name = "IMAGER"
         output_format = "pickle"
 
-    params_all = collate(telescope_name, instrument_name, detector_name, output_format)
-        self.assertEqual(collate(telescope_name), "Hello, World!")
+        self.assertEqual(collate(telescope_name, instrument_name, detector_name, output_format, save=False), params_to_compare)
 
 if __name__ == "__main__":
     unittest.main()
