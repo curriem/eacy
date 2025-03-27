9e2e428a1cafbf538cd873135157eb71d4e0e0d2
curriem <miles.h.currie@nasa.gov>
2025-03-27 15:01:10 -0400

    updating test script

--- tests/test_collate_params.py
+++ tests/test_collate_params.py
@@ -1,5 +1,6 @@
 import unittest
 from eacy import collate
+import numpy as np
 
 class TestUtils(unittest.TestCase):
     def test_collate(self):
@@ -10,7 +11,22 @@ class TestUtils(unittest.TestCase):
         detector_name = "IMAGER"
         output_format = "pickle"
 
-        self.assertEqual(collate(telescope_name, instrument_name, detector_name, output_format, save=False), params_to_compare)
+        dict1 = collate(telescope_name, instrument_name, detector_name, output_format, save=False)
+        dict2 = params_to_compare
+        are_equal = True
+        for k in dict1.keys() | dict2.keys():  # Union of keys
+            if k not in dict1 or k not in dict2:
+                are_equal = False
+                break
+            v1, v2 = dict1[k], dict2[k]
+            if isinstance(v1, np.ndarray) and isinstance(v2, np.ndarray):
+                if not np.array_equal(v1, v2):  # Use np.array_equal for array comparison
+                    are_equal = False
+                    break
+            elif v1 != v2:  # Regular comparison for non-array values
+                are_equal = False
+                break
+        self.assertTrue(are_equal, "The dictionaries are not equal.")
 
 if __name__ == "__main__":
     unittest.main()
